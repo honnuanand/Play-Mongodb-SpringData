8b6a8a10381052d3b980e980ea3791970f52f426
anand <anand@honnu.com>
2014-09-16 13:56:07 -0400

    Cleaned up some unwanted copied over code...

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,36 +8,30 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.data.mongodb.core.MongoOperations;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.index;
 
-/**
- * The main set of web services.
- */
 
 public class Application extends Controller {
 
-    /**
-     * The purpose of this method is the initial index.
-     * Need to move this into some more structured methods.
-     *
-     * @return a Result ( play.mvc.Result )
-     */
-    public Result index() {
+
+    public  Result index() {
+        return ok(index.render("Your new application is ready."));
+    }
+
+    public static Result testDataInsert() {
+
         try {
             ApplicationContext ctx =
                     new AnnotationConfigApplicationContext(MongoConfiguration.class);
             MongoOperations mongoOperation = (MongoOperations) ctx.getBean("mongoTemplate");
 
-            //Lets add  some people into the Database
             mongoOperation.save(new Person("ABCDEF", "password123"));
             mongoOperation.save(new Person("ABCDEJ", "password123"));
             mongoOperation.save(new Person("ABCDEK", "password123"));
         } catch (BeansException e) {
-            return internalServerError(e.toString());
-        } 
-
-        //return ok(views.html.index.render("Need  to Change the index page to "));
-        return ok("The Data was  inserted.");
-
-
+            internalServerError(e.toString());
+        }
+        return ok("The Data is Created");
     }
+
 }
